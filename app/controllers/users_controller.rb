7fd2e2d032f54d5a38612b6eb30c8e0146997eac
VincentJiang <jxs471494539@gmail.com>
2014-03-06 18:02:20 +0800

    admin can update heself, commom user can't; admin can update other user's role, but he can't udpate heself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,44 +31,82 @@ class UsersController < ApplicationController
   end
 
   def update
-  	@user = current_user.admin? ? User.find(params[:id]) : current_user
-		if user = User.authenticate(@user.username, params[:user][:password])
-			if params[:user][:password_new].present?
-        if params[:user][:password_new] == params[:user][:password_new_confirmation]
-  				if @user.update( email: params[:user][:email], admin: params[:user][:admin], password: params[:user][:password_new] )
-  	  			User.write_json
-            if current_user.admin
-  	  				redirect_to users_path, notice: "User #{@user.username} update successfully!"
-  	  			else
-  	  				redirect_to root_path, notice: "User #{@user.username} update successfully!"
-  	  			end
-  	  		else
-  	  			flash.now[:alert] = "User update fail"
-  					render :edit
-  	  		end
+    @user = User.find(params[:id])
+    if current_user.admin && @user == current_user
+      if User.authenticate(@user.username, params[:user][:password])
+        if params[:user][:password_new].present?
+          if params[:user][:password_new] == params[:user][:password_new_confirmation]
+            if @user.update(email: params[:user][:email], password: params[:user][:password_new], )
+              redirect_to users_path, notice: "User #{@user.username} update successfully!"
+            else
+              flash.now[:alert] = "User update fail"
+              render :edit
+            end
+          else
+            flash.now[:alert] = "Two new password is not the same."
+            render :edit
+          end
         else
-          flash.now[:alert] = "Two new password is not the same."
-          render :edit
-        end
-	  	else
-	  		@user.update(email: params[:user][:email], admin: params[:user][:admin])
-        if current_user.admin
+          @user.update(email: params[:user][:email])
           redirect_to users_path, notice: "User #{@user.username} update successfully!"
+        end
+      else
+        flash.now[:alert] = "Current password is not right!"
+        render :edit
+      end
+
+    elsif current_user.admin && @user != current_user
+      if User.authenticate(@user.username, params[:user][:password])
+        if params[:user][:password_new].present?
+          if params[:user][:password_new] == params[:user][:password_new_confirmation]
+            if @user.update(email: params[:user][:email], admin: params[:user][:admin], password: params[:user][:password_new], )
+              redirect_to users_path, notice: "User #{@user.username} update successfully!"
+            else
+              flash.now[:alert] = "User update fail"
+              render :edit
+            end
+          else
+            flash.now[:alert] = "Two new password is not the same."
+            render :edit
+          end
         else
+          @user.update(email: params[:user][:email])
+          redirect_to users_path, notice: "User #{@user.username} update successfully!"
+        end
+      else
+        flash.now[:alert] = "Current password is not right!"
+        render :edit
+      end
+        
+    else
+      if user = User.authenticate(@user.username, params[:user][:password])
+        if params[:user][:password_new].present?
+          if params[:user][:password_new] == params[:user][:password_new_confirmation]
+            if @user.update(email: params[:user][:email], password: params[:user][:password_new], )
+              redirect_to root_path, notice: "User #{@user.username} update successfully!"
+            else
+              flash.now[:alert] = "User update fail"
+              render :edit
+            end
+          else
+            flash.now[:alert] = "Two new password is not the same."
+            render :edit
+          end
+        else
+          @user.update(email: params[:user][:email])
           redirect_to root_path, notice: "User #{@user.username} update successfully!"
         end
-			end
-  	else
-      flash.now[:alert] = "User update fail"
-			render :edit
-  	end
+      else
+        flash.now[:alert] = "Current password is not right!"
+        render :edit
+      end
+    end
   end
 
   def destroy
   	@user = User.find(params[:id])
   	respond_to do |format|
   		@user.destroy
-      User.write_json
   		format.html { redirect_to users_path, notice: "User #{@user.username} delete successfully!" }
   	end
   end
